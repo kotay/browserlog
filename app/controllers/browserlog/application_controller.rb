aa74b2c1c3bd3cae558544316a8a82aad1dffc4b
kotay <ben.thompson@c4l.co.uk>
2016-02-03 21:22:21 +0000

    Update application_controller.rb

--- app/controllers/browserlog/application_controller.rb
+++ app/controllers/browserlog/application_controller.rb
@@ -1,4 +1,5 @@
 module Browserlog
   class ApplicationController < ::ApplicationController
+     http_basic_authenticate_with :name => ENV["bl_log_name"], :password => ENV["bl_log_password"]
   end
 end
